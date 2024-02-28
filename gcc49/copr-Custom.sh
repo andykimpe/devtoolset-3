bb4093d7e4804ef4176a9ba4c25f7f91883984a1
Kimpe Andy <andykimpe@gmail.com>
2024-02-28 18:10:58 +0100

    Update copr-Custom.sh

--- gcc49/copr-Custom.sh
+++ gcc49/copr-Custom.sh
@@ -1,1 +1,8 @@
-
+#! /bin/sh -x
+#use for build on
+# copr.fedorainfracloud.org
+# Mock chroot: epel-6-x86_64
+# External repositories for build dependencies
+# https://download.copr.fedorainfracloud.org/results/andykimpe/devtoolset-3/epel-6-x86_64/
+# Build dependencies: scl-utils-build wget
+wget https://github.com/andykimpe/devtoolset-3/raw/master/devtoolset-3/devtoolset-3.spec -O devtoolset-3.spec
