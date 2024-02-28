0146169df8723347c6519dd7871849d3dfc9e877
Kimpe Andy <andykimpe@gmail.com>
2024-02-28 18:42:13 +0100

    Update copr-Custom.sh

--- devtoolset-3/copr-Custom.sh
+++ devtoolset-3/copr-Custom.sh
@@ -5,5 +5,9 @@
 # External repositories for build dependencies
 # https://download.copr.fedorainfracloud.org/results/andykimpe/devtoolset-3/epel-6-x86_64/
 # Build dependencies: scl-utils-build wget
+# #! /bin/sh -x
+# wget https://github.com/andykimpe/devtoolset-3/raw/master/devtoolset-3/copr-Custom.sh -O copr-Custom.sh
+# bash copr-Custom.sh
+# rm -f copr-Custom.sh
 wget https://github.com/andykimpe/devtoolset-3/raw/master/devtoolset-3/devtoolset-3.spec -O devtoolset-3.spec
 
