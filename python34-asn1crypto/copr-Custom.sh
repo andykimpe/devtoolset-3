ca38af17c8f8764af96d226551861748aace4244
Kimpe Andy <andykimpe@gmail.com>
2024-02-29 09:56:30 +0100

    Update copr-Custom.sh

--- python34-asn1crypto/copr-Custom.sh
+++ python34-asn1crypto/copr-Custom.sh
@@ -5,7 +5,7 @@
 # Mock chroot: epal-6-x86_64
 # Build dependencies: wget
 # #! /bin/sh -x
-# wget https://github.com/andykimpe/devtoolset-3/raw/master/python34-argcomplete/copr-Custom.sh -O copr-Custom.sh
+# wget https://github.com/andykimpe/devtoolset-3/raw/master/python34-asn1crypto/copr-Custom.sh -O copr-Custom.sh
 # bash copr-Custom.sh
 # rm -f copr-Custom.sh
 wget https://dl.fedoraproject.org/pub/epel/7/SRPMS/Packages/p/python-asn1crypto-0.24.0-7.el7.src.rpm
