9dfd162559cbed63f7c3257c54972af466f71d2f
Kimpe Andy <andykimpe@gmail.com>
2024-02-29 12:48:52 +0100

    Update copr-Custom.sh

--- python34/copr-Custom.sh
+++ python34/copr-Custom.sh
@@ -10,6 +10,6 @@
 # rm -f copr-Custom.sh
 wget https://dl.fedoraproject.org/pub/epel/7/SRPMS/Packages/p/python34-3.4.10-8.el7.src.rpm -O python34-3.4.10-8.el7.src.rpm
 rpm2cpio python34-3.4.10-8.el7.src.rpm | cpio -idmv
-rm-f python34-3.4.10-8.el7.src.rpm
+rm -f python34-3.4.10-8.el7.src.rpm
 wget https://github.com/andykimpe/devtoolset-3/raw/master/python34/python34.spec -O python34.spec
 wget https://www.python.org/ftp/python/3.4.10/Python-3.4.10.tar.xz
