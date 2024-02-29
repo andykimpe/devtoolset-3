40bce2df7c4cb5436f2463c020f2e4387383ab95
Kimpe Andy <andykimpe@gmail.com>
2024-02-29 12:09:08 +0100

    Create copr-Custom.sh

---
+++ devtoolset-3-binutils/copr-Custom.sh
@@ -0,0 +1,16 @@
+#! /bin/sh -x
+#use for build on
+# copr.fedorainfracloud.org
+# add chroot with config
+# Mock chroot: epal-6-x86_64
+# Build dependencies: wget
+# #! /bin/sh -x
+# wget https://github.com/andykimpe/devtoolset-3/raw/master/devtoolset-3-binutils/copr-Custom.sh -O copr-Custom.sh
+# bash copr-Custom.sh
+# rm -f copr-Custom.sh
+wget http://archive.kernel.org/centos-vault/centos/6/sclo/Source/rh/devtoolset-3/devtoolset-3-binutils-2.24-18.el6.src.rpm
+rpm2cpio *.src.rpm | cpio -idmv
+rm-f *.src.rpm
+rm -rf devtoolset-3-binutils.spec
+rm -rf binutils.spec
+wget https://github.com/andykimpe/devtoolset-3/raw/master/devtoolset-3-binutils/devtoolset-3-binutils.spec
