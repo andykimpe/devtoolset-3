ec6220cfd78f84e78520c29f67b250e45f619727
Kimpe Andy <andykimpe@gmail.com>
2024-02-29 10:20:14 +0100

    Update and rename copr-Custom.sh to copr-Custom.sh

--- python34-backports-ssl_match_hostname/copr-Custom.sh
+++ python34-backports-ssl_match_hostname/copr-Custom.sh
@@ -5,7 +5,7 @@
 # Mock chroot: epal-6-x86_64
 # Build dependencies: wget
 # #! /bin/sh -x
-# wget https://github.com/andykimpe/devtoolset-3/raw/master/python3-backports-ssl_match_hostname/copr-Custom.sh -O copr-Custom.sh
+# wget https://github.com/andykimpe/devtoolset-3/raw/master/python34-backports-ssl_match_hostname/copr-Custom.sh -O copr-Custom.sh
 # bash copr-Custom.sh
 # rm -f copr-Custom.sh
 wget https://dl.fedoraproject.org/pub/epel/7/SRPMS/Packages/p/python3-backports-ssl_match_hostname-3.5.0.1-1.el7.src.rpm
