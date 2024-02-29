e17540c62df667603332ef62dc381a89cab56a9e
Kimpe Andy <andykimpe@gmail.com>
2024-02-29 20:05:38 +0100

    Update copr-Custom.sh

--- python3-other-rpm-macros/copr-Custom.sh
+++ python3-other-rpm-macros/copr-Custom.sh
@@ -11,5 +11,5 @@
 wget https://dl.fedoraproject.org/pub/epel/7/SRPMS/Packages/p/python-epel-rpm-macros-3-26.el7.src.rpm
 rpm2cpio *.src.rpm | cpio -idmv
 rm -f *.src.rpm
-#rm -rf *.spec
-#wget https://github.com/andykimpe/devtoolset-3/raw/master/python3-other-rpm-macros/python3-other-rpm-macros.spec
+rm -rf *.spec
+wget https://github.com/andykimpe/devtoolset-3/raw/master/python3-other-rpm-macros/python3-other-rpm-macros.spec
