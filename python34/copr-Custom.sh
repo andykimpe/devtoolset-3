4eaec056ba358375d0be7a1caa9dbaaf266c2269
Kimpe Andy <andykimpe@gmail.com>
2024-02-29 01:25:56 +0100

    Update copr-Custom.sh

--- python34/copr-Custom.sh
+++ python34/copr-Custom.sh
@@ -2,19 +2,10 @@
 #use for build on
 # copr.fedorainfracloud.org
 # add chroot with config
-# Mock chroot: custom-1-x86_64
-# add External repositories for build dependencies
-# https://archives.fedoraproject.org/pub/archive/fedora/linux/releases/23/Everything/x86_64/os/
-# https://archives.fedoraproject.org/pub/archive/fedora/linux/updates/23/x86_64/
-# https://download.copr.fedorainfracloud.org/results/yourusername/projectname/custom-1-x86_64/
-# Mock chroot: custom-1-i386
-# add External repositories for build dependencies
-# https://archives.fedoraproject.org/pub/archive/fedora/linux/releases/23/Everything/i386/os/
-# https://archives.fedoraproject.org/pub/archive/fedora/linux/updates/23/i386/
-# https://download.copr.fedorainfracloud.org/results/yourusername/projectname/custom-1-i386/
+# Mock chroot: epal-6-x86_64
 # Build dependencies: wget
 # #! /bin/sh -x
-# wget https://github.com/andykimpe/devtoolset-3/raw/master/gcc49/copr-Custom.sh -O copr-Custom.sh
+# wget https://github.com/andykimpe/devtoolset-3/raw/master/python34/copr-Custom.sh -O copr-Custom.sh
 # bash copr-Custom.sh
 # rm -f copr-Custom.sh
 wget https://github.com/andykimpe/devtoolset-3/raw/master/gcc49/gcc49.spec -O gcc49.spec
