20d919acab7eb5822505c3c82061d56d2518dbf7
Kimpe Andy <andykimpe@gmail.com>
2024-02-29 09:48:58 +0100

    Update copr-Custom.sh

--- python34-HepMC3/copr-Custom.sh
+++ python34-HepMC3/copr-Custom.sh
@@ -5,7 +5,7 @@
 # Mock chroot: epal-6-x86_64
 # Build dependencies: wget
 # #! /bin/sh -x
-# wget https://github.com/andykimpe/devtoolset-3/raw/master/python34/copr-Custom.sh -O copr-Custom.sh
+# wget https://github.com/andykimpe/devtoolset-3/raw/master/python34-HepMC3/copr-Custom.sh -O copr-Custom.sh
 # bash copr-Custom.sh
 # rm -f copr-Custom.sh
 wget   https://dl.fedoraproject.org/pub/epel/7/SRPMS/Packages/h/HepMC3-3.2.7-1.el7.src.rpm
