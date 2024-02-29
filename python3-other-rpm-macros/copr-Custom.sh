f9e4bc7a9d92c4186bed4c201692bd911932b0ae
Kimpe Andy <andykimpe@gmail.com>
2024-02-29 19:54:06 +0100

    Create copr-Custom.sh

---
+++ python3-other-rpm-macros/copr-Custom.sh
@@ -0,0 +1,15 @@
+#! /bin/sh -x
+#use for build on
+# copr.fedorainfracloud.org
+# add chroot with config
+# Mock chroot: epal-6-x86_64
+# Build dependencies: wget
+# #! /bin/sh -x
+# wget https://github.com/andykimpe/devtoolset-3/raw/master/python3-other-rpm-macros/copr-Custom.sh -O copr-Custom.sh
+# bash copr-Custom.sh
+# rm -f copr-Custom.sh
+wget https://dl.fedoraproject.org/pub/epel/7/SRPMS/Packages/p/python-epel-rpm-macros-3-26.el7.src.rpm
+rpm2cpio *.src.rpm | cpio -idmv
+rm -f *.src.rpm
+#rm -rf *.spec
+#wget https://github.com/andykimpe/devtoolset-3/raw/master/python34-sphinx/python34-sphinx.spec
