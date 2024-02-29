8bb2d4abe512bc153010ba32b735972d02ac5124
Kimpe Andy <andykimpe@gmail.com>
2024-02-29 10:23:29 +0100

    Update copr-Custom.sh

--- python34-HepMC3/copr-Custom.sh
+++ python34-HepMC3/copr-Custom.sh
@@ -10,4 +10,5 @@
 # rm -f copr-Custom.sh
 wget https://dl.fedoraproject.org/pub/epel/7/SRPMS/Packages/h/HepMC3-3.2.7-1.el7.src.rpm
 rpm2cpio *.src.rpm | cpio -idmv
-rm-f *.src.rpm
+rm-f *.src.rpm HepMC3.spec
+wget https://github.com/andykimpe/devtoolset-3/raw/master/python34-HepMC3/python34-HepMC3.spec
