3a172ab6267fb780f062c6ff48a6aa53b7d522e6
Kimpe Andy <andykimpe@gmail.com>
2024-03-01 16:56:31 +0100

    Create copr-Custom.sh

---
+++ devtoolset-3-automake15/copr-Custom.sh
@@ -0,0 +1,15 @@
+#! /bin/sh -x
+#use for build on
+# copr.fedorainfracloud.org
+# add chroot with config
+# Mock chroot: epal-6-x86_64
+# Build dependencies: wget
+# #! /bin/sh -x
+# wget https://github.com/andykimpe/devtoolset-3/raw/master/devtoolset-3-automake15/copr-Custom.sh -O copr-Custom.sh
+# bash copr-Custom.sh
+# rm -f copr-Custom.sh
+wget http://archive.kernel.org/centos-vault/centos/6/os/Source/SPackages/automake15-1.5-27.el6.1.src.rpm
+rpm2cpio *.src.rpm | cpio -idmv
+rm -f *.src.rpm
+rm -rf *.spec
+wget https://github.com/andykimpe/devtoolset-3/raw/master/devtoolset-3-automake/devtoolset-3-automake15.spec
