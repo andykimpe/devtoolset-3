78582d99e47d4582b34cb295ecd0f99eee1fefb5
Kimpe Andy <andykimpe@gmail.com>
2024-03-01 16:29:58 +0100

    Update copr-Custom.sh

--- devtoolset-3-zlib/copr-Custom.sh
+++ devtoolset-3-zlib/copr-Custom.sh
@@ -1,1 +1,15 @@
-http://archive.kernel.org/centos-vault/centos/6/os/Source/SPackages/zlib-1.2.3-29.el6.src.rpm
+#! /bin/sh -x
+#use for build on
+# copr.fedorainfracloud.org
+# add chroot with config
+# Mock chroot: epal-6-x86_64
+# Build dependencies: wget
+# #! /bin/sh -x
+# wget https://github.com/andykimpe/devtoolset-3/raw/master/devtoolset-3-zlib/copr-Custom.sh -O copr-Custom.sh
+# bash copr-Custom.sh
+# rm -f copr-Custom.sh
+wget http://archive.kernel.org/centos-vault/centos/6/os/Source/SPackages/zlib-1.2.3-29.el6.src.rpm
+rpm2cpio *.src.rpm | cpio -idmv
+rm -f *.src.rpm
+rm -rf *.spec
+wget https://github.com/andykimpe/devtoolset-3/raw/master/devtoolset-3-zlib/devtoolset-3-zlib.spec
