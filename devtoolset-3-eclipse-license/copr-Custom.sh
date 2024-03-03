9c1a0db60dec33f033ac1470b528828ecf82e406
Kimpe Andy <andykimpe@gmail.com>
2024-03-03 12:51:18 +0100

    Update copr-Custom.sh

--- devtoolset-3-eclipse-license/copr-Custom.sh
+++ devtoolset-3-eclipse-license/copr-Custom.sh
@@ -8,7 +8,7 @@
 # wget https://github.com/andykimpe/devtoolset-3/raw/master/devtoolset-3-eclipse-license/copr-Custom.sh -O copr-Custom.sh
 # bash copr-Custom.sh
 # rm -f copr-Custom.sh
-wget http://archive.kernel.org/centos-vault/centos/6/sclo/Source/rh/devtoolset-3/devtoolset-3-eclipse-jgit-3.6.1-3.bootstrap1.el6.src.rpm
+wget http://archive.kernel.org/centos-vault/centos/6/sclo/Source/rh/devtoolset-3/devtoolset-3-eclipse-license-1.0.1-5.3.bootstrap1.el6.src.rpm
 rpm2cpio *.src.rpm | cpio -idmv
 rm -f *.src.rpm
 rm -rf *.spec
