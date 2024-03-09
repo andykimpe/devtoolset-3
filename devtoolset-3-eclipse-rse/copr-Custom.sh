7da30bea6dfdae0ffa93b75d405be3106a9dbfee
Kimpe Andy <andykimpe@gmail.com>
2024-03-09 12:18:17 +0100

    Create copr-Custom.sh

---
+++ devtoolset-3-eclipse-rse/copr-Custom.sh
@@ -0,0 +1,15 @@
+#! /bin/sh -x
+#use for build on
+# copr.fedorainfracloud.org
+# add chroot with config
+# Mock chroot: epal-6-x86_64
+# Build dependencies: wget
+# #! /bin/sh -x
+# wget https://github.com/andykimpe/devtoolset-3/raw/master/devtoolset-3-eclipse-rse/copr-Custom.sh -O copr-Custom.sh
+# bash copr-Custom.sh
+# rm -f copr-Custom.sh
+wget http://archive.kernel.org/centos-vault/centos/6/sclo/Source/rh/devtoolset-3/devtoolset-3-eclipse-rse-3.6.0-8.bootstrap1.el6.src.rpm
+rpm2cpio *.src.rpm | cpio -idmv
+rm -f *.src.rpm
+rm -rf *.spec
+wget https://github.com/andykimpe/devtoolset-3/raw/master/devtoolset-3-eclipse-rse/devtoolset-3-eclipse-rse.spec
